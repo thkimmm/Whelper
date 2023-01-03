604b2e3e25cb9c77dfbc56ff04b69a6333e30d81
김태훈 <119839229+thkimmm@users.noreply.github.com>
2023-01-03 14:28:39 +0900

    staged not 상태 제거

--- Whelper/project/src/components/Write.js
+++ Whelper/project/src/components/Write.js
@@ -14,15 +14,11 @@ function Write() {
         <div className='common_page'>
             <div className='content_area'>
                 <div className=''>
-                    <div>test</div>
-                    <div>test</div>
-                    <div>test</div>
-                    <div>test</div>
-                    <div>test</div>
                     {
                         WriteListData.map(function (a, i) {
 
                             return (
+                                
                                 <div className='WriteDiv' key={i}>
 
                                     <WriteList WriteListData={WriteListData[i]} i={i}></WriteList>
